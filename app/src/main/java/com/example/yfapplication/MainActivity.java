28a22e84d951c56107204d463ee89c53189697c4
Nikolay Kashin <nikolay1s112@gmail.com>
2020-03-19 00:21:31 +0300

    add_spiner

---
+++ app/src/main/java/com/example/yfapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.yfapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, new MyFragment())
+                .commit();
+
+
+    }
+
+
+}
+
