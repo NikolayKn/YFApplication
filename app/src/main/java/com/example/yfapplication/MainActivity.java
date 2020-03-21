81cd1b7e37a34ec10a5b61c35b135578f9204e5d
nokiroki <artem.krotov42@gmail.com>
2020-03-21 18:35:36 +0300

    spinner added norm

--- app/src/main/java/com/example/yfapplication/MainActivity.java
+++ app/src/main/java/com/example/yfapplication/MainActivity.java
@@ -5,16 +5,20 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
-
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sPref;
+    final String SAVED_TEXT = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,20 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.fragment_container, new MyFragment())
                 .commit();
 
+    }
 
+    void saveText(int number) {
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putInt(SAVED_TEXT, number);
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+    }
+    int loadText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        int savedText = sPref.getInt(SAVED_TEXT, 0);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+        return savedText;
     }
 
 
--- app/src/main/java/com/example/yfapplication/MyFragment.java
+++ app/src/main/java/com/example/yfapplication/MyFragment.java
@@ -1,9 +1,17 @@
 package com.example.yfapplication;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +20,49 @@ import androidx.fragment.app.Fragment;
 import com.example.yfapplication.R;
 
 
-
 public class MyFragment extends Fragment {
+
+    String[] selected_bucket = {"Первое", "Второе", "Третье", "Четвёртое"};
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.mfragment, container, false);
+        View view = inflater.inflate(R.layout.mfragment, container, false);
+        Context context = view.getContext();
+
+        ArrayAdapter<CharSequence> Adapter = ArrayAdapter.createFromResource(context, R.array.choice, android.R.layout.simple_spinner_item);
+        Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = view.findViewById(R.id.spinner);
+
+        spinner.setAdapter(Adapter);
+        spinner.setPrompt("Title");
+
+        if (getActivity() != null){
+            MainActivity ma = (MainActivity) getActivity();
+            spinner.setSelection(ma.loadText());
+        }
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (getActivity() != null) {
+                    MainActivity ma = (MainActivity) getActivity();
+                    ma.saveText(i);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        ;
+
+
+        return view;
     }
 }
+
+
